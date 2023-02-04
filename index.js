e0ae415a2a13e399a646bfffa59c115c50e03030
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-04 08:03:52 +0300

    cors origin

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin:
-      "https://web-community-attendance-backend-production.up.railway.app",
+    origin: "https://web-community-backend.onrender.com",
   })
 );
 app.use(express.json());
