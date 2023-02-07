5e243b5a35ee53731cc99acbccb3b7d8c346cb99
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-07 23:40:48 +0300

    mongodb

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 const express = require("express");
-const mysql = require("mysql");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const cors = require("cors");
+const Attendance = require("./model/attendance");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middlewares
 app.use(
   cors({
-    origin:
-      "https://web-community-attendance-backend-production.up.railway.app",
+    origin: "*",
+    credentials: true,
+    methods: "POST",
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // connectDatabase;
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "attendance",
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error(err.message);
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, () => {
+      console.log("Database connected successfully");
+    });
+  } catch (err) {
+    console.error(err);
   }
-  console.log("Database connected successfully");
-});
+};
+connectDatabase();
 
 // submit attendance
 app.post("/attendance", async (req, res) => {
   const { firstname, lastname, year, message } = req.body;
+
   if (!(firstname || lastname || year || message)) {
     res.status(400).json({ message: "Please fill all the fields!" });
   } else {
-    let sql = "INSERT INTO attendees SET ?";
-    await connection.query(
-      sql,
-      { firstname, lastname, year, message },
-      (err, result) => {
-        if (err) throw err;
-        res.status(200).json({ message: "Attendance submitted successfully!" });
-      }
-    );
+    await Attendance.create({
+      firstname,
+      lastname,
+      year,
+      message,
+    });
+    res.status(200).json({ message: "Attendance submitted successfully!" });
   }
 });
 
