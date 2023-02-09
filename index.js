81fe2a6343dd6888481502856bd259fa8ae2e0f2
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-09 22:35:57 +0300

    configured PORT var

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const Attendance = require("./model/attendance");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
@@ -35,8 +35,8 @@ mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running at port ${port}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
     });
   })
   .catch((err) => {
