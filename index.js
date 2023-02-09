32d32bcb095cb3b13b0ddfe1cdc1e9fa38d8c597
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-09 22:06:20 +0300

    configured mongoose and app

--- index.js
+++ index.js
@@ -2,33 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
+
 const Attendance = require("./model/attendance");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    methods: "POST",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-// connectDatabase;
-const connectDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, () => {
-      console.log("Database connected successfully");
-    });
-  } catch (err) {
-    console.error(err);
-  }
-};
-connectDatabase();
 
 // submit attendance
 app.post("/attendance", async (req, res) => {
@@ -47,7 +30,15 @@ app.post("/attendance", async (req, res) => {
   }
 });
 
-// listen for server
-app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
-});
+// connectDatabase
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
