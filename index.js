1606b4c4fd75317dd201a03d8c8706ce7d6d6117
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-10 00:25:29 +0300

    cors origin url

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://web-community-attendance.netlify.app",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
